d21437f1e85ec52a373fcfe436967a30f4e4f3ed
pystudent1913 <cristiansotomayor1912@gmail.com>
2020-11-19 23:30:26 -0500

    todo lo inicial e importando el router

---
+++ my-imei/src/App.js
@@ -0,0 +1,19 @@
+// import logo from './logo.svg';
+import './App.css';
+
+import React from 'react';
+import { AppRouter } from './routers/AppRouters';
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <AppRouter />
+      </div>
+    </>
+  );
+}
+
+export default App;
