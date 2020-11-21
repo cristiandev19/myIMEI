2edf4cb560f18e12d8a54b37193b316f3bbdc58a
pystudent1913 <cristiansotomayor1912@gmail.com>
2020-11-20 23:04:53 -0500

    jugando con el grid

--- my-imei/src/components/Home/Home.js
+++ my-imei/src/components/Home/Home.js
@@ -1,18 +1,48 @@
+import { Container, Grid, makeStyles } from '@material-ui/core'
 import React from 'react'
 import { CardIMEI } from '../CardIMEI/CardIMEI'
 import { HeaderHome } from '../HeaderHome/HeaderHome'
 
-export const Home = () => {
-    return (
-        <div>
-            <HeaderHome />
-            Hola home
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    marginTop: '20px'
+  },
+  paper: {
+    height: 140,
+    width: 100,
+  },
+  control: {
+    padding: theme.spacing(2),
+  },
+}));
 
-            <CardIMEI />
-            <CardIMEI />
-            <CardIMEI />
-            <CardIMEI />
-            <CardIMEI />
-        </div>
-    )
+export const Home = () => {
+  let spacing = 4;
+  const classes = useStyles();
+
+  let array_card = [0, 1, 2,3, 6, 6];
+  return (
+    <div>
+      <HeaderHome />
+      <Container fixed>
+        {/* Hola home */}
+        <Grid container className={classes.root} spacing={2}>
+          <Grid item xs={12}>
+            <Grid container justify="center" spacing={spacing}>
+              {array_card.map((value) => (
+                <Grid key={value} item>
+                  {/* <Paper className={classes.paper} /> */}
+
+                  
+                  <CardIMEI />
+                </Grid>
+              ))}
+            </Grid>
+          </Grid>
+        </Grid>
+      </Container>
+
+    </div>
+  )
 }
