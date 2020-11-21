2f1f5aa9920d9af8ad958a53e80333d28ca3d7cc
pystudent1913 <cristiansotomayor1912@gmail.com>
2020-11-20 23:35:48 -0500

    agregue el input del buscador y estilos

--- my-imei/src/components/Home/Home.js
+++ my-imei/src/components/Home/Home.js
@@ -1,4 +1,4 @@
-import { Container, Grid, makeStyles } from '@material-ui/core'
+import { Container, Grid, makeStyles, TextField } from '@material-ui/core'
 import React from 'react'
 import { CardIMEI } from '../CardIMEI/CardIMEI'
 import { HeaderHome } from '../HeaderHome/HeaderHome'
@@ -6,7 +6,10 @@ import { HeaderHome } from '../HeaderHome/HeaderHome'
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
-    marginTop: '20px'
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    }
   },
   paper: {
     height: 140,
@@ -15,27 +18,37 @@ const useStyles = makeStyles((theme) => ({
   control: {
     padding: theme.spacing(2),
   },
+  buscador: {
+    width: '75%',
+    margin: 'auto'
+  },
+  main: {
+    marginTop: '20px',
+  }
 }));
 
 export const Home = () => {
   let spacing = 4;
   const classes = useStyles();
 
-  let array_card = [0, 1, 2,3, 6, 6];
+  let array_card = ['item_add',0, 1, 2,3, 6, 6];
   return (
     <div>
       <HeaderHome />
-      <Container fixed>
+      <Container fixed className={classes.main}>
         {/* Hola home */}
+
+        <form className={classes.buscador} noValidate autoComplete="off">
+          <TextField id="outlined-basic"  fullWidth label="Outlined" variant="outlined" />
+        </form>
         <Grid container className={classes.root} spacing={2}>
           <Grid item xs={12}>
             <Grid container justify="center" spacing={spacing}>
               {array_card.map((value) => (
+
                 <Grid key={value} item>
                   {/* <Paper className={classes.paper} /> */}
-
-                  
-                  <CardIMEI />
+                  <CardIMEI alias="hola" imei="hola" estado="hola"  type_card="value"/>
                 </Grid>
               ))}
             </Grid>
