d9a63f1ec179de246cbd39ff9c19b12495e6ee6d
pystudent1913 <cristiansotomayor1912@gmail.com>
2020-11-24 22:45:36 -0500

    agrego el context

--- my-imei/src/App.js
+++ my-imei/src/App.js
@@ -1,16 +1,18 @@
 // import logo from './logo.svg';
 import './App.css';
 
-import React from 'react';
+import React, { useState } from 'react';
 import { AppRouter } from './routers/AppRouters';
+import { AuthContext } from './auth/AuthContext';
 
 
 function App() {
+  const [user, setUser] = useState({});
 
   return (
-    <>
+    <AuthContext.Provider value={ { user, setUser } } >
       <AppRouter />
-    </>
+    </AuthContext.Provider>
   );
 }
 
