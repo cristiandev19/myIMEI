49df5ef3d82675882353b4283fd26aed7506368a
pystudent1913 <cristiansotomayor1912@gmail.com>
2020-11-25 23:23:07 -0500

    agregando cosillas

--- my-imei/src/App.js
+++ my-imei/src/App.js
@@ -1,16 +1,32 @@
 // import logo from './logo.svg';
 import './App.css';
 
-import React, { useState } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import { AppRouter } from './routers/AppRouters';
 import { AuthContext } from './auth/AuthContext';
+import { authReducer } from './auth/authReducer';
 
+
+const init = () => {
+  return JSON.parse(localStorage.getItem('user')) || { logged : false }
+}
 
 function App() {
-  const [user, setUser] = useState({});
+  // const [user, setUser] = useState({});
+  // const [user, setUser] = useState({});
+  const [user, dispatch] = useReducer(authReducer, {}, init)
+
+  useEffect(() => {
+
+    localStorage.setItem( 'user', JSON.stringify(user) );
+    // effect
+    // return () => {
+    //   cleanup
+    // }
+  }, [user])
 
   return (
-    <AuthContext.Provider value={ { user, setUser } } >
+    <AuthContext.Provider value={ { user, dispatch } } >
       <AppRouter />
     </AuthContext.Provider>
   );
